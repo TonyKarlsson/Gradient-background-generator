585c20fb43444811292ba6e67da7c694497c1493
Tony Karlsson <tonykarlsson79@gmail.com>
2021-03-11 11:30:18 +0100

    Added comments and screenshot

--- script.js
+++ script.js
@@ -6,20 +6,18 @@ let addColor = document.querySelector(".plus");
 let allDirections = document.querySelectorAll(".arrow, .radial")
 let colorPickers = document.getElementById("color-pickers");
 
-function colorPicker(direction = "bottom") {
+//  Paint background and print CSS code
+function paintBackground(direction = "bottom") {
   if (direction === "radial") {
-    body.style.background = "radial-gradient("
-    + setColor()
-    + ")";
+    body.style.background = "radial-gradient(" + readColorPicker() + ")";
   } else {
-    body.style.background = `linear-gradient(to ${direction}, `
-    + setColor()
-    + ")";
+    body.style.background = `linear-gradient(to ${direction}, ` + readColorPicker() + ")";
     }
     cssCode.textContent = body.style.background + ";"
 }
 
-function setColor() {
+// Read colorPickers and return to paintBackground
+function readColorPicker() {
   let colors = "";
   for (let n = 0; n < colorPickers.childElementCount; n++) {
     colors += colorPickers.children[n].value + ", ";
@@ -27,24 +25,26 @@ function setColor() {
   return colors.substring(0, colors.length - 2);
 }
 
-// If user clicks plus, add another colorPicker.
+// Adds colorpicker when user clicks plus
 function addColorPicker() {
   let lastColorPicker = colorPickers.lastElementChild.cloneNode(true);
   colorPickers.appendChild(lastColorPicker);
-  lastColorPicker.addEventListener("input", colorPicker);
+  lastColorPicker.addEventListener("input", paintBackground);
 };
 
+// Toggle off all active arrows or radial and toggle on last clicked, returns direction to paintBackground
 allDirections.forEach((direction) => {
   direction.addEventListener("click", (event) => {
     document.querySelectorAll(".active").forEach((activeDirection) => {
       activeDirection.classList.toggle("active")
     });
   event.currentTarget.classList.toggle("active");
-  colorPicker(event.currentTarget.dataset.direction);
+  paintBackground(event.currentTarget.dataset.direction);
   });
 });
 
-color1.addEventListener("input", colorPicker);
-color2.addEventListener("input", colorPicker);
+// Eventlisteners for the initial two color pickers and plus button
+color1.addEventListener("input", paintBackground);
+color2.addEventListener("input", paintBackground);
 addColor.addEventListener("click", addColorPicker);
   
